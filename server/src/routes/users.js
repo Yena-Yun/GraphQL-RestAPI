597b83c62fd09e389c8816f352975aacdac7ab8b
Yena-Yun <cheryleduart@gmail.com>
2022-06-24 14:55:37 +0900

    :ambulance: json-server 작업 위해 Rest API 코드로 되돌림

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,16 +1,16 @@
 // LowDB 사용하면 readDB 필요 없어짐 (Rest API에서만, GraphQL에서는 필요)
-import { readDB } from '../dbController.js';
-// import db from '../dbController.js';
+// import { readDB } from '../dbController.js';
+import db from '../dbController.js';
 
 // LowDB 사용하면 getUsers가 다음과 같이 교체됨 (Rest API)
-const getUsers = () => readDB('users');
-// const getUsers = () => {
-//   // 먼저 db 전체를 읽어옴
-//   db.read();
-//   // lowDB는 한번 불러오면 DB 데이터가 캐싱되어 있음 => 데이터가 있을 때와 없을 때를 구분 (안전장치 걸기)
-//   db.data = db.data || { users: {} }; // db.data가 없으면 users를 빈 객체로 설정
-//   return db.data.users;
-// };
+// const getUsers = () => readDB('users');
+const getUsers = () => {
+  // 먼저 db 전체를 읽어옴
+  db.read();
+  // lowDB는 한번 불러오면 DB 데이터가 캐싱되어 있음 => 데이터가 있을 때와 없을 때를 구분 (안전장치 걸기)
+  db.data = db.data || { users: {} }; // db.data가 없으면 users를 빈 객체로 설정
+  return db.data.users;
+};
 
 const usersRoute = [
   {
